65f6dcd80d5c477e93e09a3a8794097fa846b564
ryanlynch02 <72536445+ryanlynch02@users.noreply.github.com>
2020-10-07 23:26:04 -0400

    Add files via upload
    Ryan Lynch
    AddingMachine program 10/7/20 CSE 360

---
+++ AddingMachine.java
@@ -0,0 +1,41 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	
+	private int total;
+	private String words = "0";
+	
+	public AddingMachine() {
+		total = 0; //not needed - included for clarity
+	}
+	
+	public int getTotal() {
+		
+		return total;
+	}
+	
+	public void add (int value) {
+		
+		total = total + value;
+		
+		words += " + " + value;
+	}
+	
+	public void subtract (int value) {
+		
+		total = total - value;
+		
+		words += " - " + value;
+	}
+	
+	public String toString() {
+		
+		return words;
+	}
+	
+	public void clear() {
+		
+		words = "0";
+		total = 0;
+	}
+ }
